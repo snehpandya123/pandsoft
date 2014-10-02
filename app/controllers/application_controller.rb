330e9a43b51ff20aa800c0b30dac935feca91fc0
sneh <snehp92@gmail.com>
2014-10-01 21:21:31 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
+  
+
+ 	def after_sign_in_path_for(resource)
+ 		if user_signed_in?
+ 			users_dashboard_path
+ 		end
+    end
+
+	def after_sign_out_path_for(resource)
+		if user_signed_in?
+			users_dashboard_path
+ 		end
+   	end
+end
